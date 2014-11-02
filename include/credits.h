096aed751a1ad5487d08d3be6060464ffdcece77
Deversi <deversi@yahoo.gr>
2014-11-02 20:57:04 +0200

    archive
    Files Of Project1

---
+++ include/credits.h
@@ -0,0 +1,8 @@
+#ifndef _credits_common_
+#define _credits_common_
+
+char * credits_str1 = "\"Hello Everybody!I'm Deversi And This Is A Remake Of Irisman So The Credits Are Going To Me Is 25% And Only! (1/4)";
+char * credits_str2 = "This Remake On The Manager Is Just A Test For Other Projects...i Just Say To Release It Because There Are a Lot Forks Of Iris Like GameSonic Manager";
+char * credits_str3 = "Remember That I'm Not The Creator!Just i Remake It!My PSN Is Deversi , My Skype Is xcentralmmt And My Youtube Is Deversi1234 :D";
+
+#endif /* _credits_imanager_ */
